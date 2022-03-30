b22280dd48d6ef3f88fc657541ee7136a80968ca
Mick Labree <mick.labree@corvid.nl>
2022-03-30 13:11:27 +0200

    header enfooter weggehaald, werken met partials beter

--- class/DbConfig.php
+++ class/DbConfig.php
@@ -4,7 +4,7 @@ class DbConfig{
 
     public function connect(){
         try{
-            $conn = new PDO("mysql:host=localhost;dbname=hobo2022", 'root', '');
+            $conn = new PDO("mysql:host=localhost;dbname=hobo2022", 'root', 'root');
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
         }catch(PDOException $e){
