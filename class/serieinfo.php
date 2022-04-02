cc5ffde40b6a32d7a71c1ab5637ac4effa117f10
Rami <1040117@mborijnland.nl>
2022-04-02 22:24:38 +0200

    Update serieinfo.php

--- class/serieinfo.php
+++ class/serieinfo.php
@@ -10,7 +10,7 @@ class SerieInfo extends DbConfig {
     }
 
     public function getSerie(){
-        $sql = "SELECT * FROM serie WHERE SerieID>1";
+        $sql = "SELECT * FROM serie WHERE SerieID>10 ";
         $stmt = $this->connect()->prepare($sql);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_OBJ);
