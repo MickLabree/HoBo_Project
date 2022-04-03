9cd32cc2dc6604d5d6348fe4fd76a450ecad9942
Rami <1040117@mborijnland.nl>
2022-04-03 23:57:05 +0200

    .

--- zoeken.php
+++ zoeken.php
@@ -13,37 +13,95 @@
 
 <body>
 
+    <!-- Header -->
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#"><i class="fa-solid fa-magnifying-glass"></i></a></li>
+                <li><a href="#" class="active">Home</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><img style="margin: 0;" src="img/logo.png" height="60" alt="Logo"></li>
+                <li><a href="#">Series</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#" style="margin-right: 20px;"><i class="fa-solid fa-user"></i></a><a href="#"><i class="fas fa-sign-out-alt"></i></a></li>
+            </ul>
+        </nav>
+    </header>
+
   <!-- main -->
-  <form method="POST" action="zoekResultaat.php">
+  <form method="POST" action="zoeken.php">
     <input type="text" name="search" placeholder="Search"  onKeyUp='if(this.value==""){document.searchs.search.disabled="disabled";}else{document.searchs.search.disabled="";}' >
     <button type="submit" name="submit-search">Search</button>
   </form>
 
   <?php
-require_once 'class/Zoeken.php';
-$serieIns = new Zoeken();
-$zoek = new Zoeken();
+    require_once 'class/Zoeken.php';
+    $serieIns = new Zoeken();
+    $zoek = new Zoeken();
 
-if(isset($_POST["submit-search"])) {
-  foreach($zoek->zoeken($_POST['search']) as $serie){
-    echo $serie->SerieTitel . "<br>";
-  }
-}
+    if(isset($_POST["submit-search"])) {
+      // foreach($zoek->zoeken($_POST['search']) as $serie){
+
+      // }
+    }
 ?>
 
 <section id="fotos">
             <section id="flex">
-                <?php foreach($serieIns->getSerie() as $serie){ ?>
+                <?php if(isset($_POST["submit-search"])) {
+                  foreach($zoek->zoeken($_POST['search']) as $serie){?>
                     <article>
                         <a href="#" style="color: white;">
-                            <img src="img/<?= $serieIns->getImageUrl($serie->SerieID); ?>" height="300px" width="200px" alt="" onerror='this.src="img/no-image.png"'>
-                            <p><?php echo $serie->SerieTitel; ?></p>
+                        <img src="img/<?= $serieIns->getImageUrl($serie->SerieID); ?>" height="300px" width="200px" alt="" onerror='this.src="img/no-image.png"'>
+                        <p><?php echo $serie->SerieTitel; ?></p>
                         </a>
                     </article>
-                <?php } ?>
+                <?php } }?>
             </section>
   </section>
 
-</body>
 
+      <!-- Footer -->
+
+      <section id="footer">
+        <article class="footer-links">
+            <h1>About us</h1>
+        <p>
+            HoBo is free tv shows streaming website with zero ads, it allows you watch tv shows online,
+             watch tv shows online free in high quality for free. You can also download full tv shows and watch it later if you want.
+        </p>
+        <div class="item-icons">
+            <a href="#" class="fab fa-facebook" style="color: #4267B2;"></a>
+            <a href="#" class="fab fa-instagram" style="color: #517fa4;"></a>
+            <a href="#" class="fab fa-twitter" style="color: #1DA1F2;"></a>
+        </div>
+        </article>
+
+        <article class="footer-center">
+            <h1>Links</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">Series</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">Series</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                    <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">Series</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </article>
+    </section>
+
+    <script src="https://kit.fontawesome.com/148dcfd780.js" crossorigin="anonymous"></script>
+</body>
 </html>
