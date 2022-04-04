61a9087dcfc918ef543338fc1baf69749e8cc137
Rami <74742680+Rami2789@users.noreply.github.com>
2022-04-04 20:19:28 +0200

    Update serieinfo.php

--- class/serieinfo.php
+++ class/serieinfo.php
@@ -4,7 +4,7 @@ require_once 'DbConfig.php';
 class SerieInfo extends DbConfig {
 
     public function getSerie(){
-        $sql = "SELECT * FROM serie LIMIT 100";
+        $sql = "SELECT * FROM serie LIMIT 20";
         $stmt = $this->connect()->prepare($sql);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_OBJ);
