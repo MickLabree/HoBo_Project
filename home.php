78ad1131ca2ef68e5e8d796e5380a2d689af5742
Rami <74742680+Rami2789@users.noreply.github.com>
2022-04-10 17:57:08 +0200

    pagina series toegevoegd

--- home.php
+++ home.php
@@ -26,11 +26,11 @@ if(!$_SESSION['ingelogd']){
             <ul>
                 <li><a href="zoeken.php"><i class="fa-solid fa-magnifying-glass"></i></a></li>
                 <li><a href="home.php" class="active">Home</a></li>
-                <li><a href="#">Movies</a></li>
+                <li><a href="#">Movies/Series</a></li>
                 <li><img style="margin: 0;" src="img/logo.png" height="60" alt="Logo"></li>
-                <li><a href="#">Series</a></li>
                 <li><a href="contact.php">Contact</a></li>
-                <li><a href="profiel.php" style="margin-right: 20px;"><i class="fa-solid fa-user"></i></a><a href="logout.php"><i class="fas fa-sign-out-alt"></i></a></li>
+                <li><a href="profiel.php" style="margin-right: 20px;">Profile</a></li>
+                <li><a href="logout.php"><i class="fas fa-sign-out-alt"></i></a></li>
             </ul>
         </nav>
     </header>
